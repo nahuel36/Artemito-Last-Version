eb7f6ec17975fb461d3a1b87118d7a65b26e7024
hawk <nahuel36gg@gmail.com>
2025-01-07 08:49:45 -0300

    *ahora el inventario resalta el item seleccionado *se paso todo el formato de los items a un uss

--- Assets/Inventory/Editor/InventoryCustomEditor.cs
+++ Assets/Inventory/Editor/InventoryCustomEditor.cs
@@ -7,141 +7,157 @@ using static UnityEngine.GraphicsBuffer;
 using UnityEditor.UIElements;
 using Unity.Properties;
 using Unity.VisualScripting.YamlDotNet.Core.Tokens;
+using Unity.VisualScripting;
 
-namespace Artemito { 
+namespace Artemito {
 
-[CustomEditor(typeof(Inventory)), System.Serializable]
-public class InventoryCustomEditor : Editor
-{
-
-
-    private int itemSize = 75;
-
-    public override VisualElement CreateInspectorGUI()
+    [CustomEditor(typeof(Inventory)), System.Serializable]
+    public class InventoryCustomEditor : Editor
     {
-        Inventory myTarget = (Inventory)target;
+        private class ItemData {
+            public int id;
+            public Button button;
+        }
+        private List<ItemData> itemsButtonsList = new List<ItemData>();
+        private int itemSize = 75;
+        private Button selectedButton;
 
-        VisualElement root = VisualTreeAssets.Instance.inventoryView.Instantiate();
-        
-        root.RegisterCallback<GeometryChangedEvent>((evt)=>UpdateItems(root));
-
-        root.Q<VisualElement>("selected").enabledSelf = false;
-
-        Button addButton = root.Q<Button>("add");
-        addButton.clicked += () =>
+        public override VisualElement CreateInspectorGUI()
         {
-            myTarget.lastID++;
-            InventoryItem item = new InventoryItem();
-            item.id = myTarget.lastID;
-            myTarget.items.Add(item);
-            UpdateItems(root);
-        };
+            Inventory myTarget = (Inventory)target;
 
-        return root;
-    }
+            VisualElement root = VisualTreeAssets.Instance.inventoryView.Instantiate();
 
-    private void UpdateItems(VisualElement root)
-    {
-        Inventory myTarget = (Inventory)target;
+            root.Q<VisualElement>("selected").enabledSelf = false;
 
-        VisualElement itemsView = root.Q<VisualElement>("items");
-
-        itemsView.Clear();
-
-        for (int j = 0; j < myTarget.items.Count; j++)
-        {
-            InventoryItem item = myTarget.items[j];
-
-            Button button = new Button();
-            button.style.width = itemSize;
-            button.style.height = itemSize;
-            button.text = j.ToString();
-            button.style.color = new StyleColor(Color.black);
-            if (!string.IsNullOrEmpty(myTarget.items[j].name))
+            Button addButton = root.Q<Button>("add");
+            addButton.clicked += () =>
             {
-                button.text = myTarget.items[j].name;
-            }
-            if (myTarget.items[j].image != null)
-            { 
-                Background itemImage = new Background();
-                itemImage.sprite = myTarget.items[j].image;
-                button.style.backgroundImage = new StyleBackground(itemImage);
-            }
-
-            button.clicked += () =>
-            {
-                ShowSelectedPanel(root, item);
+                myTarget.lastID++;
+                InventoryItem item = new InventoryItem();
+                item.id = myTarget.lastID;
+                myTarget.items.Add(item);
+                UpdateItems(root);
             };
-            itemsView.Add(button);
-        }   
-    }
 
-    private void ShowSelectedPanel(VisualElement root, InventoryItem item)
-    {
-        // Obtén el panel de selección y actívalo
-        var selectedPanel = root.Q<VisualElement>("selected");
-        selectedPanel.enabledSelf = true;
+            VisualElement itemsView = root.Q<VisualElement>("items");
+            itemsView.Clear();
+            itemsButtonsList.Clear();
+            UpdateItems(root);
 
-        ResetSelectedPanel(root);
 
-        // Configura el campo de texto
-        var nameField = selectedPanel.Q<TextField>("name");
-        nameField.value = item.name; // Asigna el valor actual
+            return root;
+        }
 
-        nameFieldUserCallback = (evt) =>
+        private void UpdateItems(VisualElement root)
         {
-            item.name = evt.newValue; // Actualiza el nombre del modelo
-            UpdateItems(root); // Refresca la lista de botones
-        };
-        nameField.RegisterValueChangedCallback(nameFieldUserCallback);
+            Inventory myTarget = (Inventory)target;
 
-        // Configura el campo de imagen
-        var spriteField = selectedPanel.Q<ObjectField>("sprite");
-        spriteField.value = item.image; // Asigna el valor actual
+            VisualElement itemsView = root.Q<VisualElement>("items");
 
-        spriteFieldUserCallback = (evt) =>
+            for (int j = 0; j < myTarget.items.Count; j++)
+            {
+                InventoryItem item = myTarget.items[j];
+                if (itemsButtonsList.FindAll(delegate (ItemData data) { return data.id == item.id; }).Count == 0)
+                {
+                    Button button = new Button();
+                    button.text = j.ToString();
+
+                    if (!string.IsNullOrEmpty(item.name))
+                    {
+                        button.text = myTarget.items[j].name;
+                    }
+                    if (item.image != null)
+                    {
+                        button.style.backgroundImage = new StyleBackground(item.image);
+                    }
+
+                    button.AddToClassList("button");
+                    button.clicked += () =>
+                    {
+                        if (selectedButton != null)
+                        {
+                            selectedButton.RemoveFromClassList("selected");
+                        }
+                        selectedButton = button;
+                        button.AddToClassList("selected");
+                        ShowSelectedPanel(root, item);
+                    };
+
+                    itemsView.Add(button);
+                    itemsButtonsList.Add(new ItemData() { button = button, id = item.id });
+
+
+                }   
+            }
+        }
+
+        private void ShowSelectedPanel(VisualElement root, InventoryItem item)
         {
-            item.image = (Sprite)evt.newValue; // Actualiza la imagen del modelo
-            UpdateItems(root); // Refresca la lista de botones
-        };
+            // Obtén el panel de selección y actívalo
+            var selectedPanel = root.Q<VisualElement>("selected");
+            selectedPanel.enabledSelf = true;
 
-        spriteField.RegisterValueChangedCallback(spriteFieldUserCallback);
+            ResetSelectedPanel(root);
 
-        var interactionsPanel = root.Q<VisualElement>("selected").Q<VisualElement>("interactions");
+            // Configura el campo de texto
+            var nameField = selectedPanel.Q<TextField>("name");
+            nameField.value = item.name; // Asigna el valor actual
 
-        interactionsPanel.Clear();
+            nameFieldUserCallback = (evt) =>
+            {
+                item.name = evt.newValue; // Actualiza el nombre del modelo
+                UpdateItems(root); // Refresca la lista de botones
+            };
+            nameField.RegisterValueChangedCallback(nameFieldUserCallback);
 
-        if (item.interactionsVerbs == null)
-            item.interactionsVerbs = new List<InteractionVerb>();
+            // Configura el campo de imagen
+            var spriteField = selectedPanel.Q<ObjectField>("sprite");
+            spriteField.value = item.image; // Asigna el valor actual
 
-        if (item.inventoryInteractions == null)
-            item.inventoryInteractions = new List<InventoryInteraction>();
+            spriteFieldUserCallback = (evt) =>
+            {
+                item.image = (Sprite)evt.newValue; // Actualiza la imagen del modelo
+                UpdateItems(root); // Refresca la lista de botones
+            };
 
-        if(item.properties == null)
-            item.properties = new List<Property>();
+            spriteField.RegisterValueChangedCallback(spriteFieldUserCallback);
 
-        interactionsPanel.Add(InteractionVerbCustomEditor.ShowUI(item.interactionsVerbs, (Inventory)target, serializedObject));
-        interactionsPanel.Add(InventoryInteractionCustomEditor.ShowUI(item.inventoryInteractions, (Inventory)target, serializedObject));
-        interactionsPanel.Add(PropertiesCustomEditor.ShowUI(item.properties, (Inventory)target, serializedObject));
-    }
+            var interactionsPanel = root.Q<VisualElement>("selected").Q<VisualElement>("interactions");
 
-    // Almacena las referencias a los callbacks para poder desregistrarlos
-    private EventCallback<ChangeEvent<string>> nameFieldUserCallback;
-    private EventCallback<ChangeEvent<UnityEngine.Object>> spriteFieldUserCallback;
-    private void ResetSelectedPanel(VisualElement root)
-    {
-        var selectedPanel = root.Q<VisualElement>("selected");
+            interactionsPanel.Clear();
 
-        // Limpia los callbacks y valores previos de los campos
-        var nameField = selectedPanel.Q<TextField>("name");
-        if(nameFieldUserCallback!=null)
-            nameField.UnregisterValueChangedCallback(nameFieldUserCallback); // Limpia todos los callbacks previos
-        nameField.value = ""; // Resetea el valor
+            if (item.interactionsVerbs == null)
+                item.interactionsVerbs = new List<InteractionVerb>();
 
-        var spriteField = selectedPanel.Q<ObjectField>("sprite");
-        if (spriteFieldUserCallback != null)
-            spriteField.UnregisterValueChangedCallback(spriteFieldUserCallback); // Limpia todos los callbacks previos
-        spriteField.value = null; // Resetea el valor
-    }
+            if (item.inventoryInteractions == null)
+                item.inventoryInteractions = new List<InventoryInteraction>();
+
+            if(item.properties == null)
+                item.properties = new List<Property>();
+
+            interactionsPanel.Add(InteractionVerbCustomEditor.ShowUI(item.interactionsVerbs, (Inventory)target, serializedObject));
+            interactionsPanel.Add(InventoryInteractionCustomEditor.ShowUI(item.inventoryInteractions, (Inventory)target, serializedObject));
+            interactionsPanel.Add(PropertiesCustomEditor.ShowUI(item.properties, (Inventory)target, serializedObject));
+        }
+
+        // Almacena las referencias a los callbacks para poder desregistrarlos
+        private EventCallback<ChangeEvent<string>> nameFieldUserCallback;
+        private EventCallback<ChangeEvent<UnityEngine.Object>> spriteFieldUserCallback;
+        private void ResetSelectedPanel(VisualElement root)
+        {
+            var selectedPanel = root.Q<VisualElement>("selected");
+
+            // Limpia los callbacks y valores previos de los campos
+            var nameField = selectedPanel.Q<TextField>("name");
+            if(nameFieldUserCallback!=null)
+                nameField.UnregisterValueChangedCallback(nameFieldUserCallback); // Limpia todos los callbacks previos
+            nameField.value = ""; // Resetea el valor
+
+            var spriteField = selectedPanel.Q<ObjectField>("sprite");
+            if (spriteFieldUserCallback != null)
+                spriteField.UnregisterValueChangedCallback(spriteFieldUserCallback); // Limpia todos los callbacks previos
+            spriteField.value = null; // Resetea el valor
+        }
     }
 }
